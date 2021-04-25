f14e70a9164b3a168bd2ee48e194264d8bb5e13d
Wilder Medeiros <wilder.medeiros@outlook.com>
2021-04-25 19:54:04 -0300

    20 - Extracting features and moving to a txt file

--- Python Scripts/TrabalhoFinal.py
+++ Python Scripts/TrabalhoFinal.py
@@ -4,9 +4,10 @@ import numpy as np
 import pandas as pd
 
 from skimage.feature import greycomatrix, greycoprops
+from numpy import empty, mean
 
 images = [cv2.imread(file) for file in glob.glob(
-    "C:/Users/Usuario/Downloads/PDI Exercicios/Python Scripts/datasetTest/*.png")]
+    "C:/Users/Usuario/Downloads/PDI Exercicios/Python Scripts/datasetTest/*jpg")]
 
 len(images)
 
@@ -33,52 +34,68 @@ def ContournPerimeter(image):
 
 
 def GetEnergy(cooccurrenceMatrix, maxValue):
-    energy = greycoprops(cooccurrenceMatrix, 'energy')
+    energy = greycoprops(cooccurrenceMatrix, 'energy')[0][0]
     return energy
-    # energy = 0.0
-
-    # for i in range(maxValue):
-    #     for j in range(maxValue):
-    #             energy += cooccurrenceMatrix[i][j] * cooccurrenceMatrix[i][j]
-
-    # return energy
-    
 
 
 def GetHomogeneity(cooccurrenceMatrix):
-    homogeneity = greycoprops(cooccurrenceMatrix, 'homogeneity')
+    homogeneity = greycoprops(cooccurrenceMatrix, 'homogeneity')[0][0]
     return homogeneity
 
 
 def GetCorrelation(cooccurrenceMatrix):
-    correlation = greycoprops(cooccurrenceMatrix, 'correlation')
+    correlation = greycoprops(cooccurrenceMatrix, 'correlation')[0][0]
     return correlation
 
 
 def GetContrast(cooccurrenceMatrix):
-    contrast = greycoprops(cooccurrenceMatrix, 'contrast')
+    contrast = greycoprops(cooccurrenceMatrix, 'contrast')[0][0]
     return contrast
 
 
 def GetASM(cooccurrenceMatrix):
-    asm = greycoprops(cooccurrenceMatrix, 'ASM')
+    asm = greycoprops(cooccurrenceMatrix, 'ASM')[0][0]
     return asm
 
 
 def GetDissimilarity(cooccurrenceMatrix):
-    dissimilarity = greycoprops(cooccurrenceMatrix, 'dissimilarity')
+    dissimilarity = greycoprops(cooccurrenceMatrix, 'dissimilarity')[0][0]
     return dissimilarity
 
+def GetVariance(image):
+    variance = np.var(image)
+    return variance
 
+def GetMean(image):
+    myMean = mean(image)
+    return myMean 
+
+def GetMedian(image):
+    median = np.median(image)
+    return median 
+
+def GetStandartDeviantion(image):
+    standartDeviantion = np.std(image)
+    return standartDeviantion
 
 dictionaryList = []
 for image in images:
+
+    # try:
+    #     if image is None:  # The variable
+    #         print('It is None')
+    # # except NameError:
+    # #     print("This variable is not defined")
+    # # else:
+    # #     print("It is defined and has a value")
+    if image is None: 
+        pass
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #image = image / image.max()
 
     maxValue = image.max() + 1
-    cooccurrenceMatrix = greycomatrix(image, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels=maxValue)
+    cooccurrenceMatrix = greycomatrix(image, [1], [0], levels = maxValue, symmetric = False, normed = True)
 
     area = GetArea(image)
     perimeter = ContournPerimeter(image)
@@ -88,6 +105,11 @@ for image in images:
     contrast = GetContrast(cooccurrenceMatrix)
     asm = GetASM(cooccurrenceMatrix)
     dissimilarity = GetDissimilarity(cooccurrenceMatrix)
+    variance = GetVariance(image)
+    myMean = GetMean(image)
+    median = GetMedian(image)
+    standartDeviantion = GetStandartDeviantion(image)
+
     cat = 1
 
     dictionary = {
@@ -99,6 +121,10 @@ for image in images:
         "Contrast": contrast,
         "ASM": asm,
         "Dissimilarity": dissimilarity,
+        "Variance": variance,
+        "StandartDeviantion": standartDeviantion,
+        "Mean": myMean,
+        "Median": median,
         "Categoria": cat
     }
 

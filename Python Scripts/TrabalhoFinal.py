207a3eda9887ba0e5fb57b6f821edb871dfcd902
Wilder Medeiros <wilder.medeiros@outlook.com>
2021-04-25 13:47:39 -0300

    19 - Add more features to extract in the final project

--- Python Scripts/TrabalhoFinal.py
+++ Python Scripts/TrabalhoFinal.py
@@ -31,30 +31,79 @@ def ContournPerimeter(image):
 
     return perimeter
 
-# def GetEnergy(image):
+
+def GetEnergy(cooccurrenceMatrix, maxValue):
+    energy = greycoprops(cooccurrenceMatrix, 'energy')
+    return energy
+    # energy = 0.0
+
+    # for i in range(maxValue):
+    #     for j in range(maxValue):
+    #             energy += cooccurrenceMatrix[i][j] * cooccurrenceMatrix[i][j]
+
+    # return energy
+    
+
+
+def GetHomogeneity(cooccurrenceMatrix):
+    homogeneity = greycoprops(cooccurrenceMatrix, 'homogeneity')
+    return homogeneity
+
+
+def GetCorrelation(cooccurrenceMatrix):
+    correlation = greycoprops(cooccurrenceMatrix, 'correlation')
+    return correlation
+
+
+def GetContrast(cooccurrenceMatrix):
+    contrast = greycoprops(cooccurrenceMatrix, 'contrast')
+    return contrast
+
+
+def GetASM(cooccurrenceMatrix):
+    asm = greycoprops(cooccurrenceMatrix, 'ASM')
+    return asm
+
+
+def GetDissimilarity(cooccurrenceMatrix):
+    dissimilarity = greycoprops(cooccurrenceMatrix, 'dissimilarity')
+    return dissimilarity
+
 
 
 dictionaryList = []
 for image in images:
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
     #image = image / image.max()
+
+    maxValue = image.max() + 1
+    cooccurrenceMatrix = greycomatrix(image, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels=maxValue)
 
     area = GetArea(image)
     perimeter = ContournPerimeter(image)
-    #energy = GetEnergy(image)
+    energy = GetEnergy(cooccurrenceMatrix, maxValue)
+    homogeneity = GetHomogeneity(cooccurrenceMatrix)
+    correlation = GetCorrelation(cooccurrenceMatrix)
+    contrast = GetContrast(cooccurrenceMatrix)
+    asm = GetASM(cooccurrenceMatrix)
+    dissimilarity = GetDissimilarity(cooccurrenceMatrix)
     cat = 1
 
     dictionary = {
         "Area": area,
         "Perimetro": perimeter,
-        #"Corr": corr,
+        "Energy": energy,
+        "Homogeneity": homogeneity,
+        "Correlation": correlation,
+        "Contrast": contrast,
+        "ASM": asm,
+        "Dissimilarity": dissimilarity,
         "Categoria": cat
     }
+
     # get input row in dictionary format
     # key = col_name
-
     #dict1.update(blah..)
 
     dictionaryList.append(dictionary)
@@ -62,5 +111,5 @@ for image in images:
 
 df = pd.DataFrame(dictionaryList)
 
-# df.to_csv("C:/Users/Usuario/Downloads/PDI Exercicios/Python Scripts/datasetTest/dataframe.txt",
-#           header=False, sep=",", index=False)
+df.to_csv("C:/Users/Usuario/Downloads/PDI Exercicios/Python Scripts/datasetTest/dataframe.txt",
+          header=False, sep=",", index=False)
